5d71d84601c1391c14dbb3cc9517aff4e8785ea1
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-06-14 08:25:58 -0800

    Refactor: Added some comments and basic function definitions

---
+++ Arrays and Strings/question6.py
@@ -0,0 +1,14 @@
+"""
+ Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the
+  image by 90 degrees. Can you do this in place
+"""
+
+'''
+    1 2 3 4 5        1 1 1 1 1
+    1 2 3 4 5        2 2 2 2 2
+    1 2 3 4 5        3 3 3 3 3
+    1 2 3 4 5        4 4 4 4 4
+    1 2 3 4 5        5 5 5 5 5
+'''
+
+def rotate_90_degrees(matrix):
