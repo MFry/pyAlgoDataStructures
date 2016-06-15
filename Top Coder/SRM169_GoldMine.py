be4da720b38c686e6c12afff91060780713021fe
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-06-14 18:07:18 -0800

    Chore: Added additional problems to solve

---
+++ Top Coder/SRM169_GoldMine.py
@@ -0,0 +1,4 @@
+"""
+    https://community.topcoder.com/stat?c=problem_statement&pm=2235&rd=5070
+    https://www.topcoder.com/community/data-science/data-science-tutorials/greedy-is-good/
+"""
---
+++ Top Coder/SRM195_FanFailure.py
@@ -0,0 +1,7 @@
+"""
+    https://community.topcoder.com/stat?c=problem_statement&pm=2235&rd=5070
+"""
+
+
+def getRange(capacities, minCooling):
+    pass
