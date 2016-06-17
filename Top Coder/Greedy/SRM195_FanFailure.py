3263b551f4960ef0691005b51274fb610f0a06c0
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-06-17 10:41:42 -0800

    Bug: Fixed a bug where solutions were lists of int and output was tuple and incorrect mfc count

--- Top Coder/Greedy/SRM195_FanFailure.py
+++ Top Coder/Greedy/SRM195_FanFailure.py
@@ -11,6 +11,8 @@ def getRange(capacities, minCooling):
     :return:  Maximum Failure Set (MFS) and the Maximum Failure Count (MFC). A MFS is a Failure Set of fans with the
     largest count possible. A set of fans may have more than one MFS. A Failure Set is an MFS if and only
      if there are no Failure Sets with a higher count.
+     The MFC is the largest value such that all fan sets with count <= MFC are Failure Sets. In other words,
+      any set of fans of size MFC or less can fail, and the system will still be properly cooled by the remaining fans.
     """
     mfs_check = sorted(capacities)
     mfc_check = sorted(capacities, reverse=True)
@@ -26,10 +28,21 @@ def getRange(capacities, minCooling):
         if total_cooling - capacity > minCooling:
             mfc += 1
             total_cooling -= capacity
+        else:
+            # We cannot remove any more fans
+            break
     return mfs, mfc
 
 
 test_cap = [1, 2, 3]
 test_cool = 2
-solution = [2, 1]
+solution = (2, 1)
+print(getRange(test_cap, test_cool) == solution)
+test_cap = [8,5,6,7]
+test_cool = 22
+solution = (0, 0)
+print(getRange(test_cap, test_cool) == solution)
+test_cap = [676, 11, 223, 413, 823, 122, 547, 187, 28]
+test_cool = 1000
+solution = (7, 2)
 print(getRange(test_cap, test_cool) == solution)
