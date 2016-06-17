8bd2273fdd790c4636d52f5b118b0f46ac1a154b
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-06-16 19:00:21 -0800

    Feat: Completed FanFailure topcoder problem

--- Top Coder/Greedy/SRM195_FanFailure.py
+++ Top Coder/Greedy/SRM195_FanFailure.py
@@ -4,4 +4,32 @@
 
 
 def getRange(capacities, minCooling):
-    pass
+    """
+
+    :param capacities:
+    :param minCooling:
+    :return:  Maximum Failure Set (MFS) and the Maximum Failure Count (MFC). A MFS is a Failure Set of fans with the
+    largest count possible. A set of fans may have more than one MFS. A Failure Set is an MFS if and only
+     if there are no Failure Sets with a higher count.
+    """
+    mfs_check = sorted(capacities)
+    mfc_check = sorted(capacities, reverse=True)
+    total_cooling = sum(capacities)
+    mfs = 0
+    for capacity in mfs_check:
+        if total_cooling - capacity > minCooling:
+            mfs += 1
+            total_cooling -= capacity
+    mfc = 0
+    total_cooling = sum(capacities)
+    for capacity in mfc_check:
+        if total_cooling - capacity > minCooling:
+            mfc += 1
+            total_cooling -= capacity
+    return mfs, mfc
+
+
+test_cap = [1, 2, 3]
+test_cool = 2
+solution = [2, 1]
+print(getRange(test_cap, test_cool) == solution)
