0d5bf161ed35f9e8059988d160f6f9e61c4f597c
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-06-27 16:23:43 -0800

    Refactored: Added unittests and fixed a bug where max_length was returned rather than global_max_length

--- Top Coder/Dynamic Programming/TCCC03_ZigZag.py
+++ Top Coder/Dynamic Programming/TCCC03_ZigZag.py
@@ -23,22 +23,19 @@ def ZigZag(sequence):
             global_max_length = max_length
         computed_lengths[i] = (max_length, signed_diff)
 
-    return max_length
-
-
-print(ZigZag([1, 17, 5, 10, 13, 15, 10, 5, 16, 8]))
-print(ZigZag([1, 2, 3, 4, 5, 6, 7, 8, 9]))
-print(ZigZag([70, 55, 13, 2, 99, 2, 80, 80, 80, 80, 100, 19, 7, 5, 5, 5, 1000, 32, 32]))
-print(ZigZag([374, 40, 854, 203, 203, 156, 362, 279, 812, 955,
-              600, 947, 978, 46, 100, 953, 670, 862, 568, 188,
-              67, 669, 810, 704, 52, 861, 49, 640, 370, 908,
-              477, 245, 413, 109, 659, 401, 483, 308, 609, 120,
-              249, 22, 176, 279, 23, 22, 617, 462, 459, 244]))
+    return global_max_length
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_ZigZag(self):
+        self.assertTrue(7, ZigZag([1, 17, 5, 10, 13, 15, 10, 5, 16, 8]))
+        self.assertTrue(2, ZigZag([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+        self.assertTrue(8, ZigZag([70, 55, 13, 2, 99, 2, 80, 80, 80, 80, 100, 19, 7, 5, 5, 5, 1000, 32, 32]))
+        self.assertEqual(36, ZigZag([374, 40, 854, 203, 203, 156, 362, 279, 812, 955,
+                                     600, 947, 978, 46, 100, 953, 670, 862, 568, 188,
+                                     67, 669, 810, 704, 52, 861, 49, 640, 370, 908,
+                                     477, 245, 413, 109, 659, 401, 483, 308, 609, 120,
+                                     249, 22, 176, 279, 23, 22, 617, 462, 459, 244]))
 
 
 if __name__ == '__main__':
