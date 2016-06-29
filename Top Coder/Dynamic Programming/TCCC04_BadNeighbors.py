fb7d51171b1f9fc45dcc9d06cb05e4d9fd692a96
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-06-28 18:36:26 -0800

    Test: Added additional test cases and basic documentation

--- Top Coder/Dynamic Programming/TCCC04_BadNeighbors.py
+++ Top Coder/Dynamic Programming/TCCC04_BadNeighbors.py
@@ -1,5 +1,8 @@
 import unittest
 
+"""
+    Problem Statement: https://community.topcoder.com/stat?c=problem_statement&pm=2402&rd=5009
+"""
 
 def maxDonations(donations):
     computed_donations = [(0, 0)] * len(donations)
@@ -25,10 +28,11 @@ def maxDonations(donations):
 
 class MyTestCase(unittest.TestCase):
     def test_maxDonations(self):
-        self.assertTrue(19, [10, 3, 2, 5, 7, 8])
-        self.assertTrue(15, [11, 15])
-        self.assertTrue(21, [7, 7, 7, 7, 7, 7, 7])
-        self.assertTrue(16, [1, 2, 3, 4, 5, 1, 2, 3, 4, 5])
+        self.assertTrue(19, maxDonations([10, 3, 2, 5, 7, 8]))
+        self.assertTrue(15, maxDonations([11, 15]))
+        self.assertTrue(21, maxDonations([7, 7, 7, 7, 7, 7, 7]))
+        self.assertTrue(16, maxDonations([1, 2, 3, 4, 5, 1, 2, 3, 4, 5]))
+        self.assertTrue(4, maxDonations([1, 1, 0, 1, 1, 0, 1, 2, 0]))
         self.assertTrue(2926, maxDonations([94, 40, 49, 65, 21, 21, 106, 80, 92, 81, 679, 4, 61,
                                             6, 237, 12, 72, 74, 29, 95, 265, 35, 47, 1, 61, 397,
                                             52, 72, 37, 51, 1, 81, 45, 435, 7, 36, 57, 86, 81, 72]))
