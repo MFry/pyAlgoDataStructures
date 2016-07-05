85a2404646350d72859cd92b9d842e524de14ff5
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-05 13:27:25 -0800

    Chore:  Created code stubs for TODO problems

---
+++ Elements of Programming Interviews/Stacks and Queues/5.4_searching_posting_list.py
@@ -0,0 +1,9 @@
+"""
+    TODO: Write both an iterative and recursive routines
+     Assume each integer has a order field initialized to -1
+"""
+
+
+def jump_first_order(posting_list):
+    # jump first unless explored then next
+    pass
