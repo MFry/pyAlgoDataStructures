bcd8c99bc294b52691601f148129d01850736b10
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-07 09:54:17 -0800

    Feat: Changed the algorithm names to specify the brute force approach and the greedy approach and added additional test

--- Interview Prep/stock_price.py
+++ Interview Prep/stock_price.py
@@ -1,7 +1,12 @@
 import unittest
 
 
-def get_max_profits(yesterday_prices):
+def brute_get_max_profits(yesterday_prices):
+    """
+        Brute Force method
+    :param yesterday_prices:
+    :return:
+    """
     max_price = float('-inf')
     for i, buy_price in enumerate(yesterday_prices):
         best_price = float('-inf')
@@ -13,7 +18,27 @@ def get_max_profits(yesterday_prices):
     return max_price
 
 
+def get_max_profits(yesterday_prices):
+    """
+        Greedy Algorithm O(n)
+    :param yesterday_prices:
+    :return:
+    """
+    min_buy = float('inf')
+    max_price = float('-inf')
+    for sell_price in yesterday_prices:
+        if sell_price - min_buy > max_price:
+            max_price = sell_price - min_buy
+        if min_buy > sell_price:
+            min_buy = sell_price
+    return max_price
+
+
 class MyTestCase(unittest.TestCase):
     def test_get_max_profits(self):
         check_price_yesterday = [10, 7, 5, 8, 11, 9]
+        self.assertEqual(brute_get_max_profits(check_price_yesterday), 6)
         self.assertEqual(get_max_profits(check_price_yesterday), 6)
+        check_price_yesterday = [10, 11, 12, 50, 60, 100]
+        ans = brute_get_max_profits(check_price_yesterday)
+        self.assertEqual(get_max_profits(check_price_yesterday), ans)
