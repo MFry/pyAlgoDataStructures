86687d5935bb55c9a593aeb3d1e7a28ef5082c8f
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-11 11:59:06 -0800

    Chore: Added code stub and sample input for problem 6

---
+++ Interview Prep/rectangular_love.py
@@ -0,0 +1,22 @@
+"""
+Problem 6:
+"""
+
+sample_rectangle = {
+    # bottom left corner
+    'left_x': 1,
+    'bottom_y': 5,
+
+    # width and height
+    'width': 10,
+    'height': 4
+}
+
+
+def love_match(rectangle_1, rectangle_2):
+    """
+        Find the intersection between two rectangles
+    :param rectangle_1:
+    :param rectangle_2:
+    :return:
+    """
