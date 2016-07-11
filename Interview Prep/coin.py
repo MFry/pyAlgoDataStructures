b2943a385a4cff0aa42384a4f7b99d80d7901db7
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-11 11:20:51 -0800

    Feat: Completed coin count problem using a bottom up DP solution

--- Interview Prep/coin.py
+++ Interview Prep/coin.py
@@ -2,20 +2,26 @@ import unittest
 
 """
 Problem 5
-Greedy
+Dynamic Programming
 """
 
 
 def coin_permutations(amount, coins):
-
-    possible_denominations = [0] * (len(amount)+1)
+    possible_denominations = [0] * (amount + 1)
+    possible_denominations[0] = 1
 
     for coin in coins:
         for i, _ in enumerate(possible_denominations):
-            # TODO: Complete
-            # answer += num_ways(amount_remaining, number of ways)
-            pass
+            change = i - coin
+            if change >= 0:
+                possible_denominations[i] += possible_denominations[change]
 
-    return possible_denominations[amount+1]
+    return possible_denominations[amount]
 
 
+class MyTestCases(unittest.TestCase):
+    def test_coin_permutations(self):
+        coins = [1, 2, 3]
+        amount = 4
+        self.assertEqual(coin_permutations(amount, coins), 4)
+        self.assertEqual(coin_permutations(amount + 1, coins), 5)
