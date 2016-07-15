67347608f11b26a2cd42cf81f04fc6dd624bf445
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-14 16:14:08 -0800

    Feat: Initial work on finding rotation in an array

---
+++ Interview_Prep/p13_find_rottation_point.py
@@ -0,0 +1,36 @@
+"""
+ Problem 13
+
+"""
+import unittest
+
+
+def find_rotation(word_list):
+    lower_bound = 0
+    upper_bound = len(word_list) - 1
+    while True:
+        split_point = (upper_bound + lower_bound) // 2
+        if word_list[split_point] < word_list[upper_bound]:
+            upper_bound = split_point
+        if word_list[split_point] > word_list[lower_bound]:
+            lower_bound = split_point
+        if upper_bound == lower_bound:
+            return -1
+        if upper_bound - lower_bound == 2:
+            return lower_bound + 1
+
+
+class MyTestCase(unittest.TestCase):
+    def test_find_rotation(self):
+        words = ['ptomeaic',
+                 'retrograde',
+                 'supplant',
+                 'undulate',
+                 'xenoepist',
+                 'asymptote',
+                 'babka',
+                 'banoffee',
+                 'engender',
+                 'karpatka',
+                 'othellolagkage']
+        self.assertEqual(find_rotation(words), 4)
