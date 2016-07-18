d33adef40b673c69cfa160892e592963485b18af
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-17 23:03:58 -0800

    Test: Added a test case

--- Interview_Prep/p14_inflight_entertainment.py
+++ Interview_Prep/p14_inflight_entertainment.py
@@ -14,3 +14,9 @@ def find_two_movies_for_flight(flight_length, movie_lengths):
             find_movie_pairs[flight_length-movie_length] = True
     return False
 
+
+class MyTestCases(unittest.TestCase):
+    def test_find_two_movies_for_flight(self):
+        movie_length = [321, 23, 76, 96, 74, 150]
+        flight_len = 150
+        self.assertTrue(find_two_movies_for_flight(flight_len, movie_length))
