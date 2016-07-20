e747a2d0bd0550c1b1eb5d23ddd5c5ff41b38eeb
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-20 09:19:00 -0800

    Chore: Initial setup and data gathering for bloom filter problem

---
+++ misc/4_letter_words_bloom_filter.py
@@ -0,0 +1,15 @@
+"""
+Code up a simple Bloom Filter for four letter words.
+ Assume all words are lowercase, exactly four letters long, and only use the letters a to z.
+ An example set of test words might be
+  able, band, bend, card, darn, form, grab, harm, look, make, mate, peer, and team,
+  but assume the actual list of words is much bigger.
+
+Ref: https://www.careercup.com/question?id=16270664
+
+About bloom filters:
+http://billmill.org/bloomfilter-tutorial/
+http://www.i-programmer.info/programming/theory/2404-the-bloom-filter.html
+Extra python info: http://www.maxburstein.com/blog/creating-a-simple-bloom-filter/
+
+"""
