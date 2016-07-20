854f2e27b0dc86bdfecd1dd6c071f5c3b251f051
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-19 23:03:53 -0800

    Feat: Solved a greedy solution and start of dp solution for unlimited knapsack

--- Interview_Prep/p_16_cake_thief.py
+++ Interview_Prep/p_16_cake_thief.py
@@ -6,29 +6,50 @@
     Problem 16
 """
 import unittest
+import functools
 
 
 def sort_cakes(cake1, cake2):
     if cake1[0] == 0:
-        if cake1[1] > 0:
-            return 1
+        if cake1[0] > 0:
+            raise Exception("Infinite pounds generated")
         else:
             return -1
     if cake2[0] == 0:
         if cake2[1] > 0:
-            return 1
+            raise Exception("Infinite pounds generated")
         else:
             return -1
     if cake1[0]*cake2[1] > cake2[0]*cake1[1]:
-        return 0
+        return 1
     else:
-        return 0
+        return -1
+
+
+def greedy_max_duffel_bag_value(cake_tuples, capacity):
+    cake_tuples.sort(key=functools.cmp_to_key(sort_cakes))
+    british_pounds_made = 0
+    for cake_tuple in cake_tuples:
+        cakes_stolen = capacity // cake_tuple[0]
+        british_pounds_made += cake_tuple[1] * cakes_stolen
+        capacity -= cake_tuple[0] * cakes_stolen
+    return british_pounds_made
 
 
 def max_duffel_bag_value(cake_tuples, capacity):
-    cake_tuples.sort(key=sort_cakes)
-    pass
+    possible_pounds_stolen = [0] * capacity + 1
+    cake_tuples.sort(key=lambda x: x[0])
+    # check if weight is 0 and money > 0, return infinity
+    best_profit_made = 0
+    for cake_tuple in cake_tuples:
+        for pounds_stolen_per_weight in possible_pounds_stolen[cake_tuple[0]:]:
+            # check if we can make better profit at the given weight than what we have so far
+            pass
+    return best_profit_made
 
 
-
-# [(7, 160), (3, 90), (2, 15)]
+class MyTestCases(unittest.TestCase):
+    def test_max_duffel_bag_value(self):
+        cake_tuples = [(7, 160), (3, 90), (2, 15)]
+        capacity = 20
+        self.assertEqual(max_duffel_bag_value(cake_tuples, capacity), 555)
