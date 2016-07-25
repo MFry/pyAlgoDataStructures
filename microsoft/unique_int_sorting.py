3147e381e37b573e7f61bcb0a1357480c6b7b5d6
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-24 23:13:07 -0800

    Chore: Added careercup microsoft questions

---
+++ microsoft/unique_int_sorting.py
@@ -0,0 +1,7 @@
+"""
+Given an array having 16000 unique integers,
+each lying within the range 1<x<20000, how do u sort it?
+ You can load only 1000 numbers at a time in memory.
+
+Ref: https://www.careercup.com/question?id=23123665
+"""
