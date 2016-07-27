407896d7bdc5eac4b9cb1d44a03e5929153c1ae1
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-26 17:47:17 -0800

    Feat: Solved problem 25 and added test cases

--- Interview_Prep/p25_kth_to_last_node_in_singly_linked_list.py
+++ Interview_Prep/p25_kth_to_last_node_in_singly_linked_list.py
@@ -9,12 +9,30 @@ def kth_to_last_node(node, k):
     """
 
     :param node:
+    :type node:Node
     :param k:
     :return:
     """
-    return None
+    leading_node = node
+    for i in range(k):
+        leading_node = leading_node.next_node
+        if not leading_node:
+            return None
+    k_last_node = node
+    while leading_node.next_node:
+        leading_node = leading_node.next_node
+        k_last_node = k_last_node.next_node
+    return k_last_node
 
 
 class MyTestCases(unittest.TestCase):
     def test_kth_to_last_node(self):
-        pass
+        test_head = Node(1)
+        temp = test_head
+        for i in range(10):
+            temp.next_node = Node(i + 1)
+            temp = temp.next_node
+        k_th_node = kth_to_last_node(test_head, 3)
+        self.assertEqual(k_th_node.value, 7)
+        k_th_node = kth_to_last_node(k_th_node, 6)
+        self.assertEqual(k_th_node, None)
