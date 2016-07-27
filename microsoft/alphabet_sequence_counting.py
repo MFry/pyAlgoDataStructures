0d90b6e02a746bd95f1462a4e59fba3083bcb572
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-26 17:46:04 -0800

    Docs: Added an explanation for the problem

---
+++ microsoft/alphabet_sequence_counting.py
@@ -0,0 +1,31 @@
+"""
+Imagine an alphabet of words. Example:
+a ==> 1
+b ==> 2
+c ==> 3
+.
+z ==> 26
+ab ==> 27
+ac ==> 28
+.
+az ==> 51
+bc ==> 52
+and so on.
+
+Such that the sequence of characters need to be in ascending order only (ab is valid but ba is not).
+ Given any word print its index if valid and 0 if not.
+
+Input	Output
+ab	27
+ba	0
+aez	441
+
+Note: Brute-force is not allowed.
+
+Solution: http://stackoverflow.com/questions/17495167/given-a-word-print-its-index-words-can-be-increased-accordingly
+"""
+from Algorithm_and_Data_Structures_Review.binomial_coefficient import binomial_coefficient
+
+
+def alphabet_counting(string):
+    pass
