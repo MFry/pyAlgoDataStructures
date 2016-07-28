12ac644bf78dce6e6a8b15a1c1b8e2ea282edca5
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-27 17:41:23 -0800

    Feat: Created a simplified solution and tested the algorithm

--- Interview_Prep/p26_reverse_string_in_place.py
+++ Interview_Prep/p26_reverse_string_in_place.py
@@ -7,6 +7,24 @@ import unittest
 
 
 def reverse_in_place(string):
+    """
+
+    :param string:
+    :type string: str
+    :return:
+    """
+    string = list(string)
+    left_pointer = 0
+    right_pointer = len(string) - 1
+    while left_pointer < right_pointer:
+        string[left_pointer], string[right_pointer] = \
+            string[right_pointer], string[left_pointer]
+        right_pointer -= 1
+        left_pointer += 1
+    return ''.join(string)
+
+
+def alt_reverse_in_place(string):
     string = list(string)
     for i in range((len(string) // 2)):
         swap = string[i]
