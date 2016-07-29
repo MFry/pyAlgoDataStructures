b4e1e2f1bc95b79512329b565a57e2a2ec7a6066
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-29 08:11:13 -0800

    Docs: Added problem description

--- Interview_Prep/p30_permutation_palindrome.py
+++ Interview_Prep/p30_permutation_palindrome.py
@@ -1,3 +1,9 @@
 """
+Write an efficient function that checks whether any permutation ↴ of an input string is a palindrome ↴ .
+Examples:
 
+"civic" should return True
+"ivicc" should return True
+"civil" should return False
+"livci" should return False
 """
