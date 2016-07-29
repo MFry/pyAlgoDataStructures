2a00eb63d50e1d266c80428f0ef4a3a2915400f6
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-28 16:18:16 -0800

    Chore: Created new code stubs

---
+++ amazon/print_square_matrices.py
@@ -0,0 +1,4 @@
+"""
+Given a number n and k(no of swaps allowed), make the biggest number from n by making at most k swaps.
+ If its already the biggest return the number itself.
+"""
---
+++ amazon/string_pattern_check.py
@@ -0,0 +1,7 @@
+"""
+Given a string and a pattern state whether it follows the pattern.
+ For eg: s = “redblueredgreen” matches pattern “abac”
+  but not “aaab” as ‘red’ means ‘a’, ‘blue’ means ‘b’ and ‘green’ means ‘c’ here.
+String s only contains words “red”, “green” & “blue” and pattern can have any character.
+Ref: http://www.geeksforgeeks.org/amazon-interview-experience-set-276-on-campus-sde-i/
+"""
