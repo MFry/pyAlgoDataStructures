73c633fa883756308f1c8d0c9c6730be531ac72c
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-07-31 07:04:01 -0800

    Feat: Completed a solution and refactored it for efficiency

--- Interview_Prep/p30_permutation_palindrome.py
+++ Interview_Prep/p30_permutation_palindrome.py
@@ -1,5 +1,5 @@
 """
-Write an efficient function that checks whether any permutation ↴ of an input string is a palindrome ↴ .
+Write an efficient function that checks whether any permutation of an input string is a palindrome.
 Examples:
 
 "civic" should return True
@@ -7,3 +7,22 @@ Examples:
 "civil" should return False
 "livci" should return False
 """
+import unittest
+
+
+def permutation_palindrome(string):
+    unpaired_character = set()
+    for character in string:
+        if character in unpaired_character:
+            unpaired_character.remove(character)
+        else:
+            unpaired_character.add(character)
+    return len(unpaired_character) < 2
+
+
+class MyTestCases(unittest.TestCase):
+    def test_permutation_palindrome(self):
+        self.assertTrue(permutation_palindrome('civic'))
+        self.assertTrue(permutation_palindrome('ivicc'))
+        self.assertFalse(permutation_palindrome('civil'))
+        self.assertFalse(permutation_palindrome('livci'))
