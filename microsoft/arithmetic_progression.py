4c02edcc29ffff89126871ec948fd828b4e7187a
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-08-01 23:45:40 -0800

    Chore: Code stub a arithmetic progression problem

---
+++ microsoft/arithmetic_progression.py
@@ -0,0 +1,11 @@
+"""
+    Write a program to find whether a given number is a perfect square or not.
+    You can only use addition and subtraction operation to find a solution with min. complexity.
+
+    i/p : 25
+    o/p : True
+
+    i/p : 44
+    o/p: False
+    ref: https://www.careercup.com/question?id=19638671
+"""
