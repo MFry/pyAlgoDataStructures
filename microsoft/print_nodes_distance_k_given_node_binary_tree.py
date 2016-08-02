532b348d91af850b9cbf1c1d89bdad678d38ee4d
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-08-01 23:24:05 -0800

    Chore: Code stub a BST question microsoft problem

---
+++ microsoft/print_nodes_distance_k_given_node_binary_tree.py
@@ -0,0 +1,7 @@
+"""
+    Given a binary tree, a target node in the binary tree, and an integer value k,
+    print all the nodes that are at distance k from the given target node.
+    No parent pointers are available.
+
+    Ref: http://www.geeksforgeeks.org/print-nodes-distance-k-given-node-binary-tree/
+"""
