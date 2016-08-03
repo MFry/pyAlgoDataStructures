ac9c0a708ca437292e11d012e63c33e2d62c4fbe
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-08-02 23:46:29 -0800

    Chore: Code stub for duplicate find problem

---
+++ Interview_Prep/p40_find_duplicate_optimize_for_space.py
@@ -0,0 +1,15 @@
+"""
+    We have a list of integers, where:
+
+    The integers are in the range 1...n
+    The list has a length of n...n+1
+
+It follows that our list has at least one integer which appears at least twice.
+ But it may have several duplicates, and each duplicate may appear more than twice.
+
+Write a function which finds any integer that appears more than once in our list.
+
+We're going to run this function on our new, super-hip Macbook Pro With Retina Display™.
+ Thing is, the damn thing came with the RAM soldered right to the motherboard,
+ so we can't upgrade our RAM. So we need to optimize for space!
+"""
