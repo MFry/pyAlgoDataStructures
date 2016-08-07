d634c3de60286c13fbedc812618f601bd97dda2b
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-08-06 23:18:26 -0800

    Chore: Code stub for quad combinations pramp problem

---
+++ misc/quad_combinations.py
@@ -0,0 +1,13 @@
+"""
+ Given an array of numbers arr and a number S, find 4 different numbers in arr that sum up to S.
+
+    Write a function that gets arr and S and returns an array with 4 indices of such numbers in arr,
+     or null if no such combination exists.
+    Explain and code the most efficient solution possible, and analyze its runtime and space complexity.
+"""
+import unittest
+
+
+class MyTestCases(unittest.TestCase):
+    def test_quad_combinations(self):
+        pass
