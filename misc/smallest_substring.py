d7c007a2317b7a8c4ff3f49b0f9e0de312a2c0e2
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-08-09 13:03:03 -0800

    Chore: Code stub for quad combinations pramp problem

---
+++ misc/smallest_substring.py
@@ -0,0 +1,13 @@
+"""
+Given a set T of characters and a string S, find the minimum window in S which will contain all the characters in T
+
+S = “ADOBECODEBANC”
+T = “ABC”
+
+Minimum window is “BANC”.
+
+Ref:
+    http://articles.leetcode.com/finding-minimum-window-in-s-which
+
+    http://stackoverflow.com/questions/2459653/how-to-find-smallest-substring-which-contains-all-characters-from-a-given-string
+"""
