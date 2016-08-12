3a5e0dca9ebe54eaf294185226ba324ec8b0873b
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-08-11 17:50:56 -0800

    Feat: Completed problem 38

--- Interview_Prep/p37_simulate_5_sided_die.py
+++ Interview_Prep/p37_simulate_5_sided_die.py
@@ -3,3 +3,21 @@
     Use it to write a function rand5() that generates a random integer from 1 to 5.
     rand7() returns each integer with equal probability. rand5() must also return each integer with equal probability.
 """
+import unittest
+import random
+
+
+def rand7():
+    return random.randrange(1, 7)
+
+
+def rand5():
+    r = rand7()
+    if r > 5:
+        r = rand7()
+    return r
+
+
+class MyTestCases(unittest.TestCase):
+    def test_rand5(self):
+        pass
