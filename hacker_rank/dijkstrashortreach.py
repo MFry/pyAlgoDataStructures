d9919a329a05ff383e933327e3a55a4d55579328
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-08-18 16:51:59 -0800

    Feat: Solved Dijkstras graph search problem and timed it to see whether it took any performance hits.

--- hacker_rank/dijkstrashortreach.py
+++ hacker_rank/dijkstrashortreach.py
@@ -18,24 +18,24 @@ def dijkstras(graph, start_node):
         if not to_visit:
             break
         cur_node = heappop(to_visit)
-        if cur_node[1] in unvisited:
-            unvisited.remove(cur_node[1])
+        # remove the weight
+        cur_node = cur_node[1]
+        if cur_node in unvisited:
+            unvisited.remove(cur_node)
         else:
             continue
 
-        # remove the weight
-        cur_node = cur_node[1]
-
-        nodes_to_visit = graph[cur_node] if graph[cur_node] else []
-        for (node, cost) in nodes_to_visit:
+        nodes_to_visit = graph[cur_node]
+        for node, cost in nodes_to_visit.items():
             current_distance_cost = shortest_paths[cur_node] + cost
             if shortest_paths[node] > current_distance_cost:
                 shortest_paths[node] = current_distance_cost
-                heappush(to_visit, (current_distance_cost, node))
+                if node in unvisited:
+                    heappush(to_visit, (current_distance_cost, node))
 
     output = ''
     for i in range(1, len(shortest_paths)):
-        if i == float('inf'):
+        if shortest_paths[i] >= float('inf'):
             output += '-1 '
         elif i == start_node:
             continue
@@ -52,24 +52,46 @@ class MyTestCases(unittest.TestCase):
                  3: [(1, 4), (1, 3), (4, 12)],
                  4: [(3, 12)]}
         start_node = 1
-        print(dijkstras(graph, start_node))
-
-
-if __name__ == '__main__':
-    queries = int(input().strip())
-    for _ in range(queries):
-        node_count, edge_count = [int(i) for i in input().split(' ')]
-        graph = {}
-        for i in range(node_count):
-            graph[i + 1] = None
-        for i in range(edge_count):
-            node1, node2, weight = [int(i) for i in input().split(' ')]
-            connections = [(node1, node2, weight), (node2, node1, weight)]
-            # TODO: Take into account duplicate edges
-            for (key, connected_to, weight) in connections:
-                if graph[key]:
-                    graph[key].append((connected_to, weight))
-                else:
-                    graph[key] = [(connected_to, weight)]
-        start_node = int(input().strip())
-        print(dijkstras(graph, start_node))
+        # print(dijkstras(graph, start_node))
+        with open('./test_cases/dijkstras_test_case_1') as f_in, open('./test_cases/dijkstras_output_1') as f_out:
+            queries = int(f_in.readline())
+            for _ in range(queries):
+                nodes, edge_count = [int(i) for i in f_in.readline().split()]
+                graph = {}
+                for i in range(nodes):
+                    graph[i + 1] = {}
+                for _ in range(edge_count):
+                    node1, node2, weight = [int(i) for i in f_in.readline().split()]
+                    connections = [(node1, node2, weight), (node2, node1, weight)]
+                    for (key, n2, w) in connections:
+                        if n2 in graph[key]:
+                            graph[key][n2] = w if graph[key][n2] > w else graph[key][n2]
+                        else:
+                            graph[key][n2] = w
+                start_node = int(f_in.readline())
+                self.maxDiff = None
+                self.assertEqual(dijkstras(graph, start_node), f_out.readline().strip('\n'))
+        import timeit
+        start = timeit.default_timer()
+        with open('./test_cases/dijkstras_test_case_2') as f_in, open('./test_cases/dijkstras_output_2') as f_out:
+            queries = int(f_in.readline())
+            for _ in range(queries):
+                nodes, edge_count = [int(i) for i in f_in.readline().split()]
+                graph = {}
+                for i in range(nodes):
+                    graph[i + 1] = {}
+                for _ in range(edge_count):
+                    node1, node2, weight = [int(i) for i in f_in.readline().split()]
+                    connections = [(node1, node2, weight), (node2, node1, weight)]
+                    for (key, n2, w) in connections:
+                        if n2 in graph[key]:
+                            graph[key][n2] = w if graph[key][n2] > w else graph[key][n2]
+                        else:
+                            graph[key][n2] = w
+                start_node = int(f_in.readline())
+                end = timeit.default_timer()
+                print(end - start)
+                start = timeit.default_timer()
+                self.assertEqual(dijkstras(graph, start_node), f_out.readline().strip('\n'))
+                end = timeit.default_timer()
+                print(end - start)
