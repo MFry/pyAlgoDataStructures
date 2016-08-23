9a7a7e05c64b39d99578b12597382e07b08645c0
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-08-22 18:44:12 -0800

    Chore: Code stub for palindromic number problems

---
+++ geeks_for_geeks/palindromic_numbers.py
@@ -0,0 +1,14 @@
+"""
+
+    Hint: http://articles.leetcode.com/palindrome-number/
+"""
+import unittest
+
+
+def check_if_palindrome(num):
+    """
+    Ref: http://articles.leetcode.com/palindrome-number/
+    :param num:
+    :return:
+    """
+    pass
