a8396b4f540d7b203230b1fa94ef595a0ee9c20d
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-08-28 23:21:24 -0800

    Docs: Adding reference link to the problem

--- microsoft/longest_palindromic_substring.py
+++ microsoft/longest_palindromic_substring.py
@@ -1,4 +1,6 @@
 """
 Given a string, find the longest substring which is palindrome.
 For example, if the given string is “forgeeksskeegfor”, the output should be “geeksskeeg”.
+
+Ref: http://www.geeksforgeeks.org/longest-palindrome-substring-set-1/
 """
