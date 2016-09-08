c5aa4aafc1afd0a40f4538a38b9ae33d5c7578e2
Michal Frystacky <michal.frystacky@gmail.com>
2016-09-07 18:08:10 -0800

    Chore: Code stub for skip list

---
+++ Algorithm_and_Data_Structures_Review/skip_list.py
@@ -0,0 +1,7 @@
+"""
+
+  Ref:
+    https://en.wikipedia.org/wiki/Skip_list
+    http://infohost.nmt.edu/tcc/help/lang/python/examples/pyskip/web/externals.html
+    http://stackoverflow.com/questions/15121221/how-to-build-a-skip-list
+"""
