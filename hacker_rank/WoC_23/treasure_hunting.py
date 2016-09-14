6447350de184836d31497878d635cb62619bc912
Michal Frystacky <michal.frystacky@gmail.com>
2016-09-14 13:00:47 -0800

    Chore: Problem setup for line line intersect problem

---
+++ hacker_rank/WoC_23/treasure_hunting.py
@@ -0,0 +1,14 @@
+"""
+ Orthogonal vectors: http://mathworld.wolfram.com/PerpendicularVector.html
+ Line-Line Intersection: https://www.topcoder.com/community/data-science/data-science-tutorials/geometry-concepts-line-intersection-and-its-applications/
+"""
+import unittest
+
+5, 3
+1, 1
+# 4, -1
+
+100, 200
+10, 20
+# 10, 0
+# print('{:.13f}\n{:.13f}\n'.format(n,k))
