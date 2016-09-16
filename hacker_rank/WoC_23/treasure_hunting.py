4c5136a8676c0c4abdc9cc86f522681dc253033c
Michal Frystacky <michal.frystacky@gmail.com>
2016-09-15 17:28:34 -0800

    Test: Added additional test cases and added main method

--- hacker_rank/WoC_23/treasure_hunting.py
+++ hacker_rank/WoC_23/treasure_hunting.py
@@ -26,22 +26,33 @@ class MyTestCases(unittest.TestCase):
     def test_find_intersection(self):
         x, y = 5, 3
         a, b = 1, 1
-        x_i, y_i = find_intersection([(0,0), (a,b)], [(x, y), (x-a, y+b)])
-        self.assertEqual((x_i, y_i), (4,4))
-        x_i / (x_i-x-a)
-
+        x_i, y_i = find_intersection([(0, 0), (a, b)], [(x, y), (x - b, y + a)])
+        self.assertEqual((x_i, y_i), (4, 4))
+        self.assertEqual((x_i / a, (x - x_i) / -b), (4.0, -1.0))
 
         x, y = 100, 200
         a, b = 10, 20
-        x_i, y_i = find_intersection([(0, 0), (a,b)], [(x, y), (x-a, y+b)])
-        self.assertEqual((x_i, y_i), (10, 20))
+        x_i, y_i = find_intersection([(0, 0), (a, b)], [(x, y), (x - b, y + a)])
+        self.assertEqual((x_i, y_i), (100, 200))
+        self.assertEqual((x_i / a, (x - x_i) / -b), (10.0, 0.0))
+
+        x, y = 10, 20
+        a, b = 5, 4
+        x_i, y_i = find_intersection([(0, 0), (a, b)], [(x, y), (x - b, y + a)])
+        print((x_i, y_i))
+        self.assertEqual((x_i / a, (x - x_i) / -b), (3.1707317073170733, 1.4634146341463414))
+
+        x, y = 5, 5
+        a, b = 1, 1
+        x_i, y_i = find_intersection([(0, 0), (a, b)], [(x, y), (x - b, y + a)])
+        self.assertEqual((x_i / a, (x - x_i) / -b), (5.000000000000, 0.000000000000))
 
 
-5, 3
-1, 1
-# 4, -1
+if __name__ == '__main__':
+    x, y = [int(i) for i in input().split()]
+    a, b = [int(i) for i in input().split()]
 
-100, 200
-10, 20
-# 10, 0
-# print('{:.13f}\n{:.13f}\n'.format(n,k))
+    x_i, y_i = find_intersection([(0, 0), (a, b)], [(x, y), (x - b, y + a)])
+    n = x_i / a
+    k = (x - x_i) / -b
+    print('{:.13f}\n{:.13f}\n'.format(n, k))
