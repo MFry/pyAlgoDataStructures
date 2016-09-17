e6c5cffc69eceec42eae69d6042d338e89869582
Michal Frystacky <michal.frystacky@gmail.com>
2016-09-16 17:18:03 -0800

    Feat: Wrote construct_tree helper function

--- hacker_rank/WoC_23/gravity_1.py
+++ hacker_rank/WoC_23/gravity_1.py
@@ -8,14 +8,40 @@ import unittest
 
 def construct_tree(nodes, node_connections):
     tree = {}
+    for node in range(1, nodes + 1):
+        tree[node] = []
+    for i, conn in enumerate(node_connections, 1):
+        tree[conn].append(i + 1)
     return tree
 
 
 def gravity_tree(tree, u, v):
-    pass
+    distances = []
+    to_traverse = tree[v]
+    dist = 1
+    temp = []
+    while to_traverse:
+        cur = to_traverse.pop(0)
+        distances.append(dist)
+        temp.append(tree[cur])
+
+        if not to_traverse:
+            to_traverse = temp
+            dist += 1
+
+            # TODO: Finish computing the traversal distance
+            # TODO: Find the distance from u to v
 
 
 class MyTestCases(unittest.TestCase):
+    def test_construct_tree(self):
+        n = 5
+        node_connections = [1, 2, 2, 4]
+        tree = construct_tree(n, node_connections)
+        self.assertEqual(len(tree), 5)
+        self.assertEqual(len(tree[2]), 2)
+        self.assertEqual(len(tree[5]), 0)
+
     def test_gravity_tree(self):
         n = 5
         node_connections = [1, 2, 2, 4]
