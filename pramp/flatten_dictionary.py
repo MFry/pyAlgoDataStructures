ac5fb7d89f5020b283b4ba40e71d6261a0f69c32
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-09-30 22:54:11 -0800

    Docs: Added detailed problem description

--- pramp/flatten_dictionary.py
+++ pramp/flatten_dictionary.py
@@ -1,20 +1,27 @@
 """
-Input:
-{
+Given a dictionary, write a function to flatten it. Consider the following input/output scenario
+ for better understanding:
+ Input:
+ {
   'Key1': '1',
-  'Key2':
-  {
+  'Key2': {
     'a' : '2',
-    'b' : {
-      'c' : 3
+    'b' : '3',
+    'c' : {
+      'd' : '3',
+      'e' : '1'
+      }
     }
   }
-}
-Output:
-{
+ Output:
+ {
   'Key1': '1',
-  'Key2.a': '2'
-}
+  'Key2.a': '2',
+  'Key2.b' : '3',
+  'Key2.c.d' : '3',
+  'Key2.c.e' : '1'
+ }
+
 """
 
 
