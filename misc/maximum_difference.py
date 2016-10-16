84a83904c3687d0349470ce722654f141b3f5f69
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-10-15 23:59:41 -0800

    Chore: Codestub for max dif problem

---
+++ misc/maximum_difference.py
@@ -0,0 +1,17 @@
+"""
+
+"""
+import unittest
+
+
+def get_right_left_difference(arr):
+    l_start, l_stop, min = -1, -1, float('inf')
+    r_start, r_stop, max = -1, -1, float('-inf')
+    # find largest subsequence
+    cur = 0
+    for i, val in enumerate(arr):
+        if cur + val > max:
+            if r_stop + 1 != i or r_start < 0:
+                r_start = i
+            r_stop = i
+
