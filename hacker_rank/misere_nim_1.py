15f9fbdc00ed70b63e1296e733386ba5bfa8cb24
Michal Frystacky <Michal.Frystacky@alaska.gov>
2016-10-25 17:58:03 -0800

    Chore: Code stub for misere nim problem

---
+++ hacker_rank/misere_nim_1.py
@@ -0,0 +1,15 @@
+"""
+ Ref: https://www.hackerrank.com/challenges/misere-nim-1
+ Wikipedia: https://en.wikipedia.org/wiki/Nim
+ Solution hint: http://mathoverflow.net/questions/71802/analysis-of-misere-nim
+"""
+import unittest
+
+
+def misere_nim(piles):
+    pass
+
+
+class MyTestCases(unittest.TestCase):
+    def test_misere_nim(self):
+        pass
