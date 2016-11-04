579cae92dfa86009d2afea1133594089c22077e6
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-11-03 17:24:39 -0800

    Chore: Codestub for stone division problem

---
+++ hacker_rank/WoC_25/stone_division.py
@@ -0,0 +1,9 @@
+"""
+Ref: https://www.hackerrank.com/contests/w25/challenges/stone-division
+Concept Ref: http://web.mit.edu/sp.268/www/nim.pdf
+"""
+import unittest
+
+
+class MyTestCases(unittest.TestCase):
+    pass
