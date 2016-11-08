116357550e3fc88b4beb387c027866f8bc539a17
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-11-07 22:43:13 -0900

    Feat: Created a new function to filter_by_factors and added additional comments to docstrings

--- hacker_rank/WoC_25/between_two_sets.py
+++ hacker_rank/WoC_25/between_two_sets.py
@@ -1,4 +1,8 @@
 """
+gcd and lcm.
+A fact that helped me -
+If two numbers have no prime factors in common, the lowest common multiple will be equal to the product of the two numbers
+
 Ref:
 """
 import unittest
@@ -21,7 +25,22 @@ def common_factors_of_set(s, factors=set()):
     return factors
 
 
+def filter_by_factors(s, factors):
+    s = set(s)
+    to_delete = set()
+    for num in s:
+        for factor in factors:
+            if num % factor != 0:
+                to_delete.add(num)
+                break
+    return s.difference(to_delete)
+
+
 class MyTestCases(unittest.TestCase):
     def test_common_factors_of_set(self):
         factors = common_factors_of_set([16, 32, 96])
-        print(common_factors_of_set(factors, [2, 4]))
+        print(filter_by_factors(factors, [4, 2]))
+        factors = common_factors_of_set([1])
+        print(filter_by_factors(factors, [3, 5, 11]))
+        factors = common_factors_of_set([3, 4])
+        print(filter_by_factors(factors, [6]))
