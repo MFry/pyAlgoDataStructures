a1519c672e3c17d1abb7f5ff1caec29189b7b69c
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-04-06 23:51:17 -0800

    Feat: Solved the running median problem
    Implemented max heap and support methods for the class
    Implemented a working algorithm for finding a running median

--- hacker_rank/Cracking the coding interview challenge/heaps_find_the_running_median.py
+++ hacker_rank/Cracking the coding interview challenge/heaps_find_the_running_median.py
@@ -1,33 +1,111 @@
 #!/bin/python3
 
-import sys
+
 # bisect - https://docs.python.org/3.6/library/bisect.html
 # heapq - https://docs.python.org/3/library/heapq.html
-from heapq import heappush
+from heapq import heappush, heappop
+import unittest
+
+
+class Heap:
+
+    def __init__(self, max_heap=False):
+        self.max_heap = max_heap
+        self._heap = []
+
+    def push(self, val):
+        if self.max_heap:
+            val *= -1
+        heappush(self._heap, val)
+
+    def pop(self):
+        ret = heappop(self._heap)
+        if self.max_heap:
+            ret *= -1
+        return ret
+
+    def __len__(self):
+        return len(self._heap)
+
+    def __getitem__(self, key):
+        ret = self._heap[key]
+        if self.max_heap:
+            ret *= -1
+        return ret
+
+    def __bool__(self):
+        return bool(self._heap)
+
+    def __iter__(self):
+        self._i = -1
+        return self
+
+    def __next__(self):
+        if self._i + 1 < len(self._heap):
+            self._i += 1
+            return self._heap[self._i]
+        else:
+            raise StopIteration
+
+    def __str__(self):
+        to_str = '['
+        for val in self._heap:
+            if self.max_heap:
+                val *= -1
+            to_str += ' ' + str(val)
+        to_str += ' ]'
+        return to_str
 
 
 class RunningMedianContainer:
 
     def __init__(self):
-        self.max_heap = []
-        self.min_heap = []
-
-    def _add_to_max_heap(self, val):
-        val *= -1
-        heappush(self.max_heap, val)
-
-    def _get_from_max_heap(self):
-        return self.max_heap[0] * -1
+        self.max_heap = Heap(max_heap=True)
+        self.min_heap = Heap()
 
     def add_element(self, val):
         max_heap = self.max_heap
         min_heap = self.min_heap
-        if len(max_heap) > len(min_heap):
-            heappush(min_heap, val)
+        if not min_heap or min_heap[0] < val:
+            min_heap.push(val)
+        else:
+            max_heap.push(val)
+        self._rebalance()
+
+    def _rebalance(self):
+        large_heap = self.min_heap if len(self.min_heap) > len(self.max_heap) else self.max_heap
+        small_heap = self.min_heap if len(self.min_heap) < len(self.max_heap) else self.max_heap
+        if len(large_heap) - len(small_heap) > 1:
+            small_heap.push(large_heap.pop())
+
+    def getMedian(self):
+        max_heap = self.max_heap
+        min_heap = self.min_heap
+        if len(max_heap) == len(min_heap):
+            return (max_heap[0]+min_heap[0])/2
+        elif len(min_heap) > len(max_heap):
+            return min_heap[0]
+        else:
+            return max_heap[0]
+
+
+class MyTestCases(unittest.TestCase):
+    def test_running_median(self):
+        medianContainer = RunningMedianContainer()
+        medianContainer.add_element(1)
+        self.assertEqual(1, medianContainer.getMedian())
+        medianContainer.add_element(2)
+        self.assertEqual(1.5, medianContainer.getMedian())
+        medianContainer.add_element(3)
+        self.assertEqual(2, medianContainer.getMedian())
+        medianContainer.add_element(4)
+        self.assertEqual(2.5, medianContainer.getMedian())
 
 
 if __name__ == '__main__':
     n = int(input().strip())
     a = []
+    runningMedianContainer = RunningMedianContainer()
     for _ in range(n):
-        a.append(int(input().strip()))
+        runningMedianContainer.add_element(int(input().strip()))
+        print(runningMedianContainer.getMedian())
